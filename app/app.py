1f47579ddcc1a989df32973fd1e11e27042caa13
syagura <syahrulgr@gmail.com>
2024-12-13 00:48:48 +0700

    feat: update app.py and add new template for user login

--- app/app.py
+++ app/app.py
@@ -1,12 +1,110 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, session, redirect, make_response
+from apscheduler.schedulers.background import BackgroundScheduler
+from dotenv import load_dotenv
+import os 
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+from flask_session import Session
+import secrets
+from threading import Lock
+from flask_socketio import SocketIO, emit
+
 
 app = Flask(__name__)
 
 app.secret_key = 'secretkeyhere'
 
-@app.route('/')
+socketio = SocketIO(app)
+thread = None
+thread_lock = Lock()
+scheduler = BackgroundScheduler()
+
+load_dotenv()
+
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST")
+
+app.config['MYSQL_HOST'] = {DB_HOST}
+app.config['MYSQL_USER'] = {DB_USERNAME}
+app.config['MYSQL_PASSWORD'] = {DB_PASSWORD}
+app.config['MYSQL_DB'] = {DB_NAME}
+
+app.config["SESSION_PERMANENT"] = False
+app.config['SESSION_TYPE'] = "filesystem"
+Session(app)
+
+mysql = MySQL(app)
+users = []
+
+def get_user_by_sid(sid):
+    for user in users:
+        if user['sid'] == sid:
+            return user
+    print(f"No user found for sid: {sid}")
+    return None
+
+@app.before_request
+def before_request():
+    if 'session_id' in request.cookies:
+        sid = request.cookies.get('session_id')
+        user_data = get_user_by_sid(sid)
+        if user_data:
+            print(f"User Data: {user_data}")
+            session['id'] = user_data.get('id')
+            session['username'] = user_data.get('username')
+            session['role'] = user_data.get('role')
+        else:
+            print("No user data found.")
+            session.clear() 
+
+@app.route('/', methods=['POST', 'GET'])
 def login():
-    return render_template('auth/login.html', title='Login')
+    msg = ''
+    if request.method == 'POST' and 'NIP-NIM' in request.form and 'password' in request.form:
+        session.pop('user_id', None)
+        user_id = request.form['NIP-NIM']
+        password = request.form['password']
+        cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cur.execute('SELECT * FROM lecturer WHERE nip = % s \
+                    AND password = % s', (user_id, password))
+        account = cur.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['nip']
+            session['username'] = account['lecture_name']
+            session['role'] = account['role']
+            sid = secrets.token_hex(16)
+            session['sid'] = sid
+            users.append({'id' : session['id'], 'username' : session['username'], 'role' : session['role'], 'sid':sid})
+            print(f'{users}')
+
+            response = make_response(redirect(url_for('home')))
+            response.set_cookie('session_id', sid)
+            return response
+            
+        else:
+            cur.execute('SELECT * FROM student WHERE nim = % s\
+                        AND password = % s', (user_id, password))
+            account = cur.fetchone()
+            if account:
+                session['loggedin'] = True
+                session['id'] = account['nim']
+                session['username'] = account['student_name']
+                session['role'] = account['role']
+                sid = secrets.token_hex(16)
+                session['sid'] = sid
+                users.append({'id' : session['id'], 'username' : session['username'], 'role' : session['role'], 'sid':sid})
+                print(f'{users}')
+
+                response = make_response(redirect(url_for('home')))
+                response.set_cookie('session_id', sid)
+                return response
+            else:
+                msg = 'Logged in failed please check NIM or password !'
+
+    return render_template('auth/login.html', title='Login', msg=msg)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app,debug=True)
